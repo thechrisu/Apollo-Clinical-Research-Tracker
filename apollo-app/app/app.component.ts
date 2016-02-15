0fa1b968c8da551f4530c178345c137d71652170
TimboKZ <tima_kuzh@inbox.ru>
2016-02-15 17:18:19 +0000

    Cleaned up the repo a bit

--- apollo-app/app/app.component.ts
+++
@@ -1,7 +0,0 @@
-import {Component} from 'angular2/core';
-
-@Component({
-    selector: 'apollo-app',
-    template: '<h1>Apollo Angular2 App</h1>'
-})
-export class AppComponent { }
--- mockup/templates/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Timur Kuzhagaliyev
- * Date: 09/02/2016
- * Time: 09:51
- */
