15e7b4cfa1af0bfe66d3e338819c7917608c634e
TimboKZ <tima_kuzh@inbox.ru>
2016-02-15 18:53:35 +0000

    Added documentation, introduced namespaces

--- apollo-app/apollo/Apollo.php
+++ apollo-app/apollo/Apollo.php
@@ -1,22 +1,57 @@
 <?php
-use Apollo\Components\Request;
-
 /**
- * Created on 15/02/2016 at 17:02
+ * Main Apollo application class file
+ *
  * @author Timur Kuzhagaliyev <tim.kuzh@gmail.com>
  * @copyright 2016
  * @license http://opensource.org/licenses/gpl-license.php MIT License
  * @version 0.0.1
  */
 
+
+namespace Apollo;
+use Apollo\Components\Request;
+
+
+/**
+ * Class Apollo
+ *
+ * Main Apollo class responsible for creating the request object and directing it to
+ * the appropriate controller.
+ *
+ * @author Timur Kuzhagaliyev <tim.kuzh@gmail.com>
+ * @since 0.0.1
+ */
 class Apollo
 {
 
+    /**
+     * Object containing the request information
+     *
+     * @var Request
+     */
     private $request;
 
-    public function start() {
-
+    /**
+     * Apollo constructor.
+     *
+     * Populates the class variables
+     *
+     * @since 0.0.1
+     */
+    public function __construct()
+    {
         $this->request = new Request();
+    }
+
+    /**
+     * Initialises the application by parsing the request
+     * @access public
+     * @since 0.0.1
+     */
+    public function start()
+    {
+
 
     }
 
