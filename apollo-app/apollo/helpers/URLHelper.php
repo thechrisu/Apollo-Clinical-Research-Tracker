96a552104cd150aadcb0a75cf5f181a0759deb9a
TimboKZ <tima_kuzh@inbox.ru>
2016-02-15 18:52:01 +0000

    Created a url helper file

---
+++ apollo-app/apollo/helpers/URLHelper.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * @author Timur Kuzhagaliyev <tim.kuzh@gmail.com>
+ * @copyright 2016
+ * @license http://opensource.org/licenses/gpl-license.php MIT License
+ * @version 0.0.1
+ */
+
+
+namespace Apollo\Helpers;
+
+
+/**
+ * Class URLHelper
+ *
+ * Contains various function related to URL parsing and composing
+ *
+ * @package Apollo\Helpers
+ * @author Timur Kuzhagaliyev <tim.kuzh@gmail.com>
+ * @since 0.0.1
+ */
+class URLHelper
+{
+
+    /**
+     * Method to split the url into parts, stripping the base url before hand
+     *
+     * @param string $url
+     * @return array
+     */
+    public static function split($url) {
+
+        //TODO: Split the url
+
+    }
+
+
+    /**
+     * Method to strip the base url or the leading slash
+     *
+     * @param string $url
+     * @return string
+     */
+    public static function stripBase($url) {
+
+        //TODO: Strip the base url or the leading slash
+
+    }
+
+}
