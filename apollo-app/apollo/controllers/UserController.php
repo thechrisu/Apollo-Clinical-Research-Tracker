4ec6aaac10951609fbfeee757ffc5185ce58d344
TimboKZ <tima_kuzh@inbox.ru>
2016-02-16 21:34:34 +0000

    Added basic user authorisation

--- apollo-app/apollo/controllers/UserController.php
+++ apollo-app/apollo/controllers/UserController.php
@@ -7,7 +7,14 @@
 
 
 namespace Apollo\Controllers;
+
 use Apollo\Apollo;
+use Apollo\Components\DB;
+use Apollo\Components\Session;
+use Apollo\Components\View;
+use Apollo\Entities\UserEntity;
+use Apollo\Helpers\URLHelper;
+use Doctrine\ORM\EntityRepository;
 
 
 /**
@@ -23,7 +30,62 @@ class UserController implements GenericController
      * Default User action, simply redirects to sign in screen
      * @since 0.0.1
      */
-    public function index() {
-        Apollo::getInstance()->getRequest()->sendTo('user/sign-in/');
+    public function index()
+    {
+        $query = Apollo::getInstance()->getRequest()->getQuery();
+        Apollo::getInstance()->getRequest()->sendTo('user/sign-in/' . ($query ? '?' . $query : ''));
+    }
+
+    /**
+     * Action to handle the sign in, both the view and the POST request
+     */
+    public function actionSignIn()
+    {
+        if (!Apollo::getInstance()->getUser()->isGuest()) {
+            Apollo::getInstance()->getRequest()->sendToIndex();
+        }
+
+        $data = [
+            'error' => null
+        ];
+
+        if (isset($_POST['email']) && isset($_POST['password'])) {
+            /**
+             * @var EntityRepository $user_repository
+             */
+            $user_repository = DB::getEntityManager()->getRepository('\\Apollo\\Entities\\UserEntity');
+            /**
+             * @var UserEntity $user
+             */
+            $user = $user_repository->findOneBy(['email' => strtolower($_POST['email'])]);
+            if ($user != null) {
+                if (password_verify($_POST['password'], $user->getPassword())) {
+                    //TODO: Perhaps make this more secure?
+                    Session::set('fingerprint', Session::getFingerprint(md5($user->getPassword())));
+                    Session::set('user_id', $user->getId());
+                    if (isset($_GET['return'])) {
+                        Apollo::getInstance()->getRequest()->sendTo($_GET['return'], false);
+                    } else {
+                        Apollo::getInstance()->getRequest()->sendToIndex();
+                    }
+                } else {
+                    $data = [
+                        'error' => 'Invalid email/password combination.'
+                    ];
+                }
+            } else {
+                $data = [
+                    'error' => 'Invalid email/password combination.'
+                ];
+            }
+        }
+
+        echo View::getView()->make('user.sign-in', ['data' => $data])->render();
+    }
+
+    public function actionSignOut()
+    {
+        Session::destroy();
+        Apollo::getInstance()->getRequest()->sendToIndex();
     }
 }
