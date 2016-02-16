a403b92d463c9cb2b0a9ec1f8d9a410dd61bb154
TimboKZ <tima_kuzh@inbox.ru>
2016-02-16 12:32:56 +0000

    Added database constants

--- apollo-app/apollo/Constants.php
+++ apollo-app/apollo/Constants.php
@@ -13,5 +13,11 @@
 
 /**
  * The base url of the website with a slash "/" in the end
+ * @since 0.0.1
  */
 define('BASE_URL', 'http://82.0.141.65/');
+
+define('DB_HOST', '127.0.0.1');
+define('DB_NAME', 'apollo');
+define('DB_USER', 'root');
+define('DB_PASS', '');
