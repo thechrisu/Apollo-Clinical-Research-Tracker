ed53d6fe8a59c5ebe125ca82d9b392e8aa02dde4
TimboKZ <tima_kuzh@inbox.ru>
2016-02-16 15:33:31 +0000

    Created cache and views folders for Blade, as well as a basic template

---
+++ apollo-app/apollo/views/signin.blade.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Sign in page which all unauthorised users will see.
+ *
+ * @author Timur Kuzhagaliyev <tim.kuzh@gmail.com>
+ * @copyright 2016
+ * @license http://opensource.org/licenses/gpl-license.php MIT License
+ * @version 0.0.1
+ */
+?>
+
+<h1>Hello!</h1>
