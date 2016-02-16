63bf2137f8ac2988c5b5fbf83ae9f11ca2d0d3ad
TimboKZ <tima_kuzh@inbox.ru>
2016-02-16 18:48:27 +0000

    Added proper request parsing, as in the request are now being directed to appropriate controller and actions within said controllers

--- apollo-app/apollo/Apollo.php
+++ apollo-app/apollo/Apollo.php
@@ -12,6 +12,7 @@ namespace Apollo;
 use Apollo\Components\Request;
 use Apollo\Components\User;
 use Apollo\Components\View;
+use Apollo\Controllers\GenericController;
 
 
 /**
@@ -67,18 +68,46 @@ class Apollo
     }
 
     /**
-     * Initialises the application by parsing the request
+     * Initialises the application by parsing the request and directing it to an appropriate
+     * controller and an appropriate action inside said controller.
      * @access public
+     * @since 0.0.2 Proper controller/action parsing
      * @since 0.0.1
      */
     public function start()
     {
         if($this->user->isGuest()) {
-            if($this->request->getController() != 'User') {
-                $this->request->sendTo('user/signin/?return=' . $this->request->getUrl(), false);
+            if($this->request->getController() != 'User' || $this->request->getAction() != 'SignIn') {
+                $this->request->sendTo('user/sign-in/?return=' . $this->request->getUrl(), false);
             }
         }
-        echo View::getView()->make('error', ['error' => '404', 'error_message' => 'Page Not Found!']);
+        // Check that the requested controller exists
+        $controller_path = __DIR__ . '/controllers/' . $this->request->getController() . 'Controller.php';
+        if(file_exists($controller_path)) {
+            $controller_class = 'Apollo\\Controllers\\' . $this->request->getController() . 'Controller';
+            /**
+             * @var GenericController $controller_instance
+             */
+            $controller_instance = new $controller_class();
+            if(!$this->request->hasAction()) {
+                $controller_instance->index();
+            } else {
+                $action_name = 'action' . $this->request->getAction();
+                // Check that the requested action exists
+                if(method_exists($controller_instance, $action_name)) {
+                    $controller_instance->$action_name();
+                } else {
+                    $this->error('404', 'Page not found! (Action ' . $this->request->getAction() . ' not found in Controller ' . $this->request->getController() . ')');
+                }
+            }
+        } else {
+            $this->error('404', 'Page not found! (Controller ' . $this->request->getController() . ' not found)');
+        }
+
+    }
+
+    public function error($error, $message) {
+        echo View::getView()->make('error', ['error' => $error, 'error_message' => $message])->render();
     }
 
     /**
