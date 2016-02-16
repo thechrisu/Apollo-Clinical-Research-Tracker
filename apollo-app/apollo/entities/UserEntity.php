962b23c7d8fbf086f5f37d55a582fde276228125
TimboKZ <tima_kuzh@inbox.ru>
2016-02-16 12:45:06 +0000

    Created a UserEntity class for users table

---
+++ apollo-app/apollo/entities/UserEntity.php
@@ -0,0 +1,170 @@
+<?php
+/**
+ * @author Timur Kuzhagaliyev <tim.kuzh@gmail.com>
+ * @copyright 2016
+ * @license http://opensource.org/licenses/gpl-license.php MIT License
+ */
+
+
+namespace Apollo\Entities;
+use DateTime;
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\Id;
+
+
+/**
+ * Class UserEntity
+ *
+ * @package Apollo\Entities
+ * @author Timur Kuzhagaliyev <tim.kuzh@gmail.com>
+ * @version 0.0.1
+ * @Entity @Table(name="users")
+ */
+class UserEntity
+{
+    /**
+     * Unique user ID
+     * @var int
+     * @Id @Column(type="integer") @GeneratedValue
+     */
+    protected $id;
+
+    /**
+     * Unique user email
+     * @var string
+     * @Column(type="string")
+     */
+    protected $email;
+
+    /**
+     * User password hash
+     * @var string
+     * @Column(type="string")
+     */
+    protected $password;
+
+    /**
+     * ID of the organisation that user belongs to
+     * @var int
+     * @Column(type="integer")
+     */
+    protected $org_id;
+
+    /**
+     * Boolean indicating if the user is an admin
+     * @var bool
+     * @Column(type="boolean")
+     */
+    protected $is_admin;
+
+    /**
+     * Date that the user has registered on
+     * @var DateTime
+     * @Column(type="datetime")
+     */
+    protected $registered_on;
+
+    public function __construct()
+    {
+        $this->is_admin = false;
+        $this->registered_on = new DateTime();
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @return int
+     */
+    public function getOrgId()
+    {
+        return $this->org_id;
+    }
+
+    /**
+     * @param int $org_id
+     */
+    public function setOrgId($org_id)
+    {
+        $this->org_id = $org_id;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isIsAdmin()
+    {
+        return $this->is_admin;
+    }
+
+    /**
+     * @param boolean $is_admin
+     */
+    public function setIsAdmin($is_admin)
+    {
+        $this->is_admin = $is_admin;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getRegisteredOn()
+    {
+        return $this->registered_on;
+    }
+
+    /**
+     * @param DateTime $registered_on
+     */
+    public function setRegisteredOn($registered_on)
+    {
+        $this->registered_on = $registered_on;
+    }
+
+}
