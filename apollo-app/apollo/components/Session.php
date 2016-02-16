06b775e17f9c2798b3d7596c63e419154d354ac4
TimboKZ <tima_kuzh@inbox.ru>
2016-02-16 21:39:34 +0000

    Added documentation and version information

--- apollo-app/apollo/components/Session.php
+++ apollo-app/apollo/components/Session.php
@@ -24,19 +24,35 @@ class Session
      *
      * @param string $salt
      * @return string
+     * @since 0.0.1
      */
     public static function getFingerprint($salt = '') {
         return md5($_SERVER['HTTP_USER_AGENT'] . $salt . $_SERVER['REMOTE_ADDR']);
     }
 
+    /**
+     * Assigns the value to a key in the session
+     *
+     * @param string $key
+     * @param string $value
+     */
     public static function set($key, $value) {
         $_SESSION[$key] = $value;
     }
 
+    /**
+     * Returns the value of the key or null if it is not set
+     *
+     * @param string$key
+     * @return string
+     */
     public static function get($key) {
         return isset($_SESSION[$key]) ? $_SESSION[$key] : null;
     }
 
+    /**
+     * Destroy the session and unset all variables
+     */
     public static function destroy() {
         session_unset();
         session_destroy();
