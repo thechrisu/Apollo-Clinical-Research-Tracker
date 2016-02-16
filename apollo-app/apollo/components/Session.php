cd7ab2cd3a662c3246cbf6711403717cf32b8ce5
TimboKZ <tima_kuzh@inbox.ru>
2016-02-16 21:33:15 +0000

    Added a wrapper for some session-related functions

---
+++ apollo-app/apollo/components/Session.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @author Timur Kuzhagaliyev <tim.kuzh@gmail.com>
+ * @copyright 2016
+ * @license http://opensource.org/licenses/gpl-license.php MIT License
+ */
+
+
+namespace Apollo\Components;
+
+
+/**
+ * Class Session
+ *
+ * @package Apollo\Components
+ * @author Timur Kuzhagaliyev <tim.kuzh@gmail.com>
+ * @version 0.0.1
+ */
+class Session
+{
+    /**
+     * Generates a fingerprint for the user based on their IP and user agent.
+     * Optional $salt can be specified to make the function a bit more secure.
+     *
+     * @param string $salt
+     * @return string
+     */
+    public static function getFingerprint($salt = '') {
+        return md5($_SERVER['HTTP_USER_AGENT'] . $salt . $_SERVER['REMOTE_ADDR']);
+    }
+
+    public static function set($key, $value) {
+        $_SESSION[$key] = $value;
+    }
+
+    public static function get($key) {
+        return isset($_SESSION[$key]) ? $_SESSION[$key] : null;
+    }
+
+    public static function destroy() {
+        session_unset();
+        session_destroy();
+    }
+}
