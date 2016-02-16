3606154b8d30e70f5a441132ec4930e0dfdaabff
TimboKZ <tima_kuzh@inbox.ru>
2016-02-16 13:24:22 +0000

    Added constants for components and APP_DIR constant

--- apollo-app/apollo/Constants.php
+++ apollo-app/apollo/Constants.php
@@ -7,7 +7,7 @@
  * @author Timur Kuzhagaliyev <tim.kuzh@gmail.com>
  * @copyright 2016
  * @license http://opensource.org/licenses/gpl-license.php MIT License
- * @version 0.0.2
+ * @version 0.0.3
  */
 
 
@@ -16,6 +16,20 @@
  * @since 0.0.1
  */
 define('BASE_URL', 'http://82.0.141.65/');
+
+/**
+ * Absolute path to the 'apollo' folder
+ * @since 0.0.3
+ */
+define('APP_DIR', __DIR__);
+
+/**
+ * Various relative paths to folder for components
+ * @since 0.0.3
+ */
+define('DOCTRINE_ENTITIES_PATH', '/entities');
+define('BLADE_VIEWS_PATH', '/views');
+define('BLADE_CACHE_PATH', '/cache');
 
 /**
  * Constants for database connection, self-explanatory
