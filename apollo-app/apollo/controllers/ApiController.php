d03988bcb47ba8dc6c36bd1cd0abe2e0f7c9798d
TimboKZ <tima_kuzh@inbox.ru>
2016-03-01 10:16:52 +0000

    Added a temporary API placeholder

--- apollo-app/apollo/controllers/ApiController.php
+++ apollo-app/apollo/controllers/ApiController.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * @author Desislava Koleva <desy.koleva96@gmail.com>
+ * @author Timur Kuzhagaliyev <tim.kuzh@gmail.com>
  * @copyright 2016
  * @license http://opensource.org/licenses/mit-license.php MIT License
  */
@@ -9,6 +10,7 @@ namespace Apollo\Controllers;
 
 
 use Apollo\Apollo;
+use Apollo\Components\DB;
 
 /**
  * Class ApiController
@@ -17,12 +19,13 @@ use Apollo\Apollo;
  *
  * @package Apollo\Controllers
  * @author Desislava Koleva <desy.koleva96@gmail.com>
- * @version 0.0.1
+ * @version 0.0.2
  */
 class ApiController extends GenericController
 {
     /**
-     * Default action
+     * Since an empty request to API is not valid, return 400 error
+     *
      * @since 0.0.1
      */
     public function index()
@@ -31,78 +34,53 @@ class ApiController extends GenericController
     }
 
     /**
-     * Not found action
+     * Request to a non-existent action is invalid, return 400
+     *
      * @since 0.0.1
      */
     public function notFound()
     {
-        $request = Apollo::getInstance()->getRequest();
-        $request->error(400, 'Requested action does not exist.');
+        Apollo::getInstance()->getRequest()->error(400, 'Requested action does not exist.');
     }
 
     /**
      * Action to handle get requests
+     *
+     * @param string $table
+     * @param string $order
+     * @param string $page
+     * @param string $search
+     * @since 0.0.2 Refactored completely, added arguments
      * @since 0.0.1
      */
-    public function actionGet() // url/api/get/
+    public function actionGet($table = null, $order = null, $page = null, $search = null)
     {
         $request = Apollo::getInstance()->getRequest();
-        $parameters = $request->getParameters();
-        if (count($parameters) == 0) {
-            $request->error(400, 'No parameters were specified.');
-        } else {
-            switch ($parameters[0]) {
-                case 'records':
-                    getRecords();
-                    break;
-                case 'fields':
-                    getFields();
-                    break;
-                case 'programs':
-                    getPrograms();
-                    break;
-                case 'program':
-                    getProgram();
-                    break;
-                case 'awards':
-                    getAwards();
-                    break;
-                case 'publications':
-                    getPublications();
-                    break;
-                default:
-                    $request->error(400, 'Invalid first parameter.');
-            }
+        $table = strtolower($table);
+        if (empty($table)) {
+            $request->error(400, 'No table name specified.');
+            return;
         }
-    }
-
-    public function getRecords()
-    {
-
-    }
-
-    public function getFields()
-    {
-
-    }
-
-    public function getPrograms()
-    {
-
-    }
-
-    public function getProgram()
-    {
-
-    }
-
-    public function getAwards()
-    {
-
-    }
-
-    public function getPublications()
-    {
-
+        if (!in_array($table, ['records'])) {
+            $request->error(400, 'Invalid table requested.');
+            return;
+        }
+        echo '<pre>';
+        $data = [];
+        $data['error'] = null;
+        $people = DB::getEntityManager()->createQuery('SELECT u.id, u.given_name, u.last_name FROM Apollo\\Entities\\PersonEntity u');
+        $people = $people->getResult();
+        $record = DB::getEntityManager()->createQuery('SELECT u.id FROM Apollo\\Entities\\RecordEntity u');
+        $record = $record->getResult()[0]['id'];
+        $phone = DB::getEntityManager()->createQuery("SELECT u.varchar FROM Apollo\\Entities\\DataEntity u WHERE u.record = :record_id AND u.field = 1");
+        $phone->setParameter('record_id', $record);
+        $phone = $phone->getResult()[0]['varchar'];
+        $email = DB::getEntityManager()->createQuery("SELECT u.varchar FROM Apollo\\Entities\\DataEntity u WHERE u.record = :record_id AND u.field = 2");
+        $email->setParameter('record_id', $record);
+        $email = $email->getResult()[0]['varchar'];
+        $people[0]['phone'] = $phone;
+        $people[0]['email'] = $email;
+        $data['data'] = $people;
+        echo json_encode($data);
     }
 }
