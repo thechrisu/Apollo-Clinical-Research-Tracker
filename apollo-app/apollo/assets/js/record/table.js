630370d6bda0ea6bbb0cc419bde4dc88a43f9ef3
Tim Kuzhagaliyev <tim.kuzh@gmail.com>
2016-03-02 10:22:32 +0000

    Added redirect on click, added a pointer cursor effect on hover, recompiled JS

--- apollo-app/apollo/assets/js/record/table.js
+++ apollo-app/apollo/assets/js/record/table.js
@@ -1,14 +1,13 @@
 ///<reference path="../ajax.ts"/>
 $(document).ready(function () {
-    var page = 1;
-    updateTable();
-    function updateTable() {
-        ajaxGet(location.origin + '/api/get/records/' + page + '/', function (data) {
+    updateTable(1);
+    function updateTable(page) {
+        ajaxGet(location.origin + '/api/get/records/?page=' + page, function (data) {
             var table = $('#table-body');
             table.html('');
             for (var i = 0; i < data.data.length; i++) {
                 var record = data.data[i];
-                var tr = $('<tr></tr>');
+                var tr = $('<tr style="cursor: pointer" class="record-tr" data-id="' + record.id + '"></tr>');
                 tr.append('<td>' + record.given_name + '</td>');
                 tr.append('<td>' + record.last_name + '</td>');
                 tr.append('<td>' + record.email + '</td>');
@@ -22,7 +21,10 @@ $(document).ready(function () {
     $('#pagination').on('click', 'a', function () {
         var that = $(this);
         var _page = parseInt(that.text());
-        page = _page;
-        updateTable();
+        updateTable(_page);
+    });
+    $('#table-body').on('click', '.record-tr', function () {
+        var that = $(this);
+        window.location = location.origin + '/record/view/' + that.data('id') + '/';
     });
 });
