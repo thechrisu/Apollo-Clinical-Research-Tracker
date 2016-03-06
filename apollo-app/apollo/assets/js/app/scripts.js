46136c07357242c0db06ac2ad495fb56102dee93
TimboKZ <tima_kuzh@inbox.ru>
2016-03-06 19:36:54 +0000

    Moved helper function to Util class

--- apollo-app/apollo/assets/js/app/scripts.js
+++ apollo-app/apollo/assets/js/app/scripts.js
@@ -5,8 +5,48 @@
  * @author Timur Kuzhagaliyev <tim.kuzh@gmail.com>
  * @copyright 2016
  * @license http://opensource.org/licenses/mit-license.php MIT License
- * @version 0.0.3
+ * @version 0.0.4
  */
+/**
+ * Util class
+ * @since 0.0.4
+ */
+var Util = (function () {
+    function Util() {
+    }
+    /**
+     * Returns the full URL to resource
+     *
+     * @returns {string}
+     * @since 0.0.4
+     */
+    Util.url = function (url, trailingSlash) {
+        if (trailingSlash === void 0) { trailingSlash = true; }
+        if (url.substr(0, 1) != '/') {
+            url = '/' + url;
+        }
+        if (trailingSlash && url.substr(url.length - 1) != '/') {
+            url += '/';
+        }
+        return url;
+    };
+    /**
+     * Displays the error modal window
+     *
+     * @param message
+     * @since 0.0.4
+     */
+    Util.error = function (message) {
+        if (message === void 0) { message = 'An error has occurred.'; }
+        var modal = $('#error-modal');
+        var messageContainer = $('#error-message');
+        modal.on('show.bs.modal', function () {
+            messageContainer.html(message);
+        });
+        modal.modal('show');
+    };
+    return Util;
+})();
 /**
  * Sets the base url
  * @since 0.0.1
@@ -19,6 +59,7 @@ if (!location.origin) {
  *
  * @returns {string}
  * @since 0.0.2
+ * @deprecated
  */
 function url(url, trailingSlash) {
     if (trailingSlash === void 0) { trailingSlash = true; }
@@ -35,6 +76,7 @@ function url(url, trailingSlash) {
  *
  * @param message
  * @since 0.0.2
+ * @deprecated
  */
 function error(message) {
     if (message === void 0) { message = 'An error has occurred.'; }
--- apollo-app/apollo/assets/js/app/scripts.ts
+++ apollo-app/apollo/assets/js/app/scripts.ts
@@ -5,7 +5,7 @@
  * @author Timur Kuzhagaliyev <tim.kuzh@gmail.com>
  * @copyright 2016
  * @license http://opensource.org/licenses/mit-license.php MIT License
- * @version 0.0.3
+ * @version 0.0.4
  */
 
 /**
@@ -15,6 +15,44 @@
 interface Error {
     id: number,
     description: string
+}
+
+/**
+ * Util class
+ * @since 0.0.4
+ */
+class Util {
+
+    /**
+     * Returns the full URL to resource
+     *
+     * @returns {string}
+     * @since 0.0.4
+     */
+    public static url(url:string, trailingSlash:boolean = true) {
+        if (url.substr(0, 1) != '/') {
+            url = '/' + url;
+        }
+        if (trailingSlash && url.substr(url.length - 1) != '/') {
+            url += '/';
+        }
+        return url;
+    }
+
+    /**
+     * Displays the error modal window
+     *
+     * @param message
+     * @since 0.0.4
+     */
+    public static error(message:string = 'An error has occurred.') {
+        var modal = $('#error-modal');
+        var messageContainer = $('#error-message');
+        modal.on('show.bs.modal', function () {
+            messageContainer.html(message);
+        });
+        modal.modal('show');
+    }
 }
 
 /**
@@ -30,6 +68,7 @@ if (!location.origin) {
  *
  * @returns {string}
  * @since 0.0.2
+ * @deprecated
  */
 function url(url:string, trailingSlash:boolean = true) {
     if (url.substr(0, 1) != '/') {
@@ -46,6 +85,7 @@ function url(url:string, trailingSlash:boolean = true) {
  *
  * @param message
  * @since 0.0.2
+ * @deprecated
  */
 function error(message:string = 'An error has occurred.') {
     var modal = $('#error-modal');
