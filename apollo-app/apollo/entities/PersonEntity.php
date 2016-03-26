b7d37d4fdfbff16af6d5262bf76fa8fbd586d97b
Christoph Ulshoefer <christoph.ulshoefer@yahoo.de>
2016-03-26 18:04:28 +0100

    For some reason, entity re-adding didn't work

--- apollo-app/apollo/entities/PersonEntity.php
+++ apollo-app/apollo/entities/PersonEntity.php
@@ -1,6 +1,162 @@
 <?php
 /**
  * @author Christoph Ulshoefer <christophsulshoefer@gmail.com>
+ * @author Timur Kuzhagaliyev <tim.kuzh@gmail.com>
  * @copyright 2016
- * @license http://opensource.org/licenses/gpl-license.php MIT License
+ * @license http://opensource.org/licenses/mit-license.php MIT License
  */
+namespace Apollo\Entities;
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\ManyToOne;
+use Doctrine\ORM\Mapping\OneToMany;
+use Doctrine\ORM\Mapping\OrderBy;
+/**
+ * Class PersonEntity
+ * @package Apollo\Entities
+ * @author Christoph Ulshoefer <christophsulshoefer@gmail.com>
+ * @author Timur Kuzhagaliyev <tim.kuzh@gmail.com>
+ * @version 0.0.5
+ * @Entity @Table("people")
+ */
+class PersonEntity
+{
+    /**
+     * Unique Person id
+     * @Id @Column(type="integer") @GeneratedValue
+     * @var int
+     */
+    protected $id;
+    /**
+     * Name of the person
+     * @Column(type="string")
+     * @var string
+     */
+    protected $given_name;
+    /**
+     * Middle Name of the person
+     * @Column(type="string")
+     * @var string
+     */
+    protected $middle_name;
+    /**
+     * Last name of the person
+     * @Column(type="string")
+     * @var string
+     */
+    protected $last_name;
+    /**
+     * Organisation object
+     * @ManyToOne(targetEntity="OrganisationEntity")
+     * @var OrganisationEntity
+     */
+    protected $organisation;
+    /**
+     * Array with all of the records
+     * @OneToMany(targetEntity="RecordEntity", mappedBy="person")
+     * @var RecordEntity[]
+     */
+    protected $records;
+    /**
+     * Determines if person is shown or not
+     * @Column(type="boolean")
+     * @var bool
+     */
+    protected $is_hidden = false;
+    public function __construct()
+    {
+        $this->is_hidden = false;
+    }
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+    /**
+     * @return string
+     */
+    public function getGivenName()
+    {
+        return $this->given_name;
+    }
+    /**
+     * @param string $given_name
+     */
+    public function setGivenName($given_name)
+    {
+        $this->given_name = $given_name;
+    }
+    /**
+     * @return string
+     */
+    public function getMiddleName()
+    {
+        return $this->middle_name;
+    }
+    /**
+     * @param string $middle_name
+     */
+    public function setMiddleName($middle_name)
+    {
+        $this->middle_name = $middle_name;
+    }
+    /**
+     * @return string
+     */
+    public function getLastName()
+    {
+        return $this->last_name;
+    }
+    /**
+     * @param string $last_name
+     */
+    public function setLastName($last_name)
+    {
+        $this->last_name = $last_name;
+    }
+    /**
+     * @return OrganisationEntity
+     */
+    public function getOrganisation()
+    {
+        return $this->organisation;
+    }
+    /**
+     * @param OrganisationEntity $organisation
+     */
+    public function setOrganisation($organisation)
+    {
+        $this->organisation = $organisation;
+    }
+    /**
+     * @return RecordEntity[]
+     */
+    public function getRecords()
+    {
+        return $this->records;
+    }
+    /**
+     * @param RecordEntity $record
+     */
+    public function addRecord($record)
+    {
+        $this->records[] = $record;
+    }
+    /**
+     * @return boolean
+     */
+    public function isHidden()
+    {
+        return $this->is_hidden;
+    }
+    /**
+     * @param boolean $is_hidden
+     */
+    public function setIsHidden($is_hidden)
+    {
+        $this->is_hidden = $is_hidden;
+    }
+}
